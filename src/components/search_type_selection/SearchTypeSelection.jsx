a9522d65e836f4b057ce5ac9d57dbb237f437907
9045857 <9045857@mail.ru>
2023-11-10 00:27:43 +0700

    добавил серый фон и подчеркивание типов поиска по клику

--- src/components/search_type_selection/SearchTypeSelection.jsx
+++ src/components/search_type_selection/SearchTypeSelection.jsx
@@ -1,11 +1,32 @@
+import { useState } from 'react';
 import './searchTypeSelection.css';
 
 export const SearchTypeSelection = ({ cb = Function.prototype }) => {
+    const [tipeSelection, setTypeSelection] = useState('goods'); //goods или manufacturers
+
     return (
         <>
             <div className='search-type'>
-                <span className='selected-search-type'>Товары</span>
-                <span>Производители</span>
+                <span
+                    className={
+                        tipeSelection === 'goods'
+                            ? 'selected-search-type'
+                            : null
+                    }
+                    onClick={() => setTypeSelection('goods')}
+                >
+                    Товары
+                </span>
+                <span
+                    className={
+                        tipeSelection === 'manufacturers'
+                            ? 'selected-search-type'
+                            : null
+                    }
+                    onClick={() => setTypeSelection('manufacturers')}
+                >
+                    Производители
+                </span>
             </div>
         </>
     );
