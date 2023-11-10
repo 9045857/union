efcba7565dbd8281e9cf72a035056dfeb1a79fdf
9045857 <9045857@mail.ru>
2023-11-10 21:35:28 +0700

    горизонтальная прогрутка сделана через нажатия, пока нет ограничений на сдвиг. Нужно сделать, что бы сильно далеко не убегало

--- src/components/categoris_producer/CategoryProducer.jsx
+++ src/components/categoris_producer/CategoryProducer.jsx
@@ -1,15 +1,26 @@
 import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
 
 export const CatogoryProducer = (props) => {
     const {
         id = props.idMeal,
         image = props.strMealThumb,
         title = props.strMeal,
+        left = props.left,
     } = props;
+
+    useEffect(() => {
+        console.log(left);
+        return;
+    }, [left]);
 
     return (
         <Link to={`/meal/${id}`}>
-            <div className='card category-producer'>
+            <div
+                className='card category-producer'
+                // style={{ left: { left } }}
+                style={{ left: left }}
+            >
                 <div className='card-image image-category-producer'>
                     <img
                         src={image}
@@ -18,7 +29,7 @@ export const CatogoryProducer = (props) => {
                 </div>
                 <div className='card-content category-producer-content-title'>
                     <span className='category-producer-title card-title black-text'>
-                        {title.substr(0,30)}
+                        {title.substr(0, 30)}
                     </span>
                 </div>
             </div>
