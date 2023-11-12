ffb5d09f20b78c2b4c9d202eee2d7a41490583c3
9045857 <9045857@mail.ru>
2023-11-12 10:35:06 +0700

    дизайн блоков образцов доделан на троечку. Пока сойдет. Фиксируем минимальный размер, убрали бордер каждой карты, добавили отступы

--- src/components/samples_container/SamplesContainer.jsx
+++ src/components/samples_container/SamplesContainer.jsx
@@ -2,17 +2,24 @@ import { useState, useEffect } from 'react';
 import { Sample } from './Sample';
 import './samplesContainer.css';
 
+//компнент показывает 4 элемента из массива в отдельном блоке
 export const SamplesContainer = ({ title = '', samples = [] }) => {
     //const [categories, setCategories] = useState([]);
-    // const [samples, setSamples] =
-    //     useState([]);
+    const [items, setItems] = useState([]);
 
+    // useEffect(() => {
+    //     console.log(samples);
+    // }, [samples]);
+
+    // метод при загрузке выбирирает 4 случаных элемента из массива
     useEffect(() => {
-        console.log(samples);
+        const arrayLenght = samples.length;
+        //TODO сделать случайное наполнение массива из сэмлов. Случайную цену, но что бы она не менялась. заполнить массив объектов.
     }, [samples]);
 
     return (
         <>
+
             <div className='samples-container'>
                 <dir className='samples-container-title'>
                     <h6>{title}</h6>
@@ -27,6 +34,6 @@ export const SamplesContainer = ({ title = '', samples = [] }) => {
                     ))}
                 </dir>
             </div>
-        </>
+          </>
     );
 };
