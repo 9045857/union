75ba3de956c10fefd1c3aefd54c3fe790531c9f2
9045857 <9045857@mail.ru>
2023-11-13 22:59:51 +0700

    в качестве разделителя в ценах сайт выбирает точку или запятую исходя из настроек пользователя

--- src/components/samples_container/SamplesContainer.jsx
+++ src/components/samples_container/SamplesContainer.jsx
@@ -2,53 +2,55 @@ import { useState, useEffect } from 'react';
 import { Sample } from './Sample';
 import './samplesContainer.css';
 
-//компнент показывает 4 элемента из массива в отдельном блоке
 export const SamplesContainer = ({ title = '', samples = [] }) => {
-    //const [categories, setCategories] = useState([]);
     const [items, setItems] = useState([]);
 
-    function getRandomElements(arr, numElements) {
-        const shuffled = arr.slice().sort(() => 0.5 - Math.random()); // копируем и перемешиваем массив
-        return shuffled.slice(0, numElements); // возвращаем первые numElements элементов
-    }
+    const getRandomElements = (arr, numElements) => {
+        const shuffled = arr.slice().sort(() => 0.5 - Math.random());
+        return shuffled.slice(0, numElements);
+    };
 
-    function addRandomPrice(array) {
+    const addRandomPrice = (array) => {
+        const userLocale = navigator.language;
+        const formatter = new Intl.NumberFormat(userLocale);
         const newArray = array.map((item) => ({
             ...item,
-            price: `${(
-                Math.floor(Math.random() * (10000 - 30 + 1)) / 100 +
-                30
-            ).toFixed(2)} руб.`,
+            price: `${formatter.format(
+                (
+                    Math.floor(Math.random() * (10000 - 30 + 1)) / 100 +
+                    30
+                ).toFixed(2)
+            )} руб.`,
         }));
 
         return newArray;
-    }
-    // метод при загрузке выбирирает 4 случаных элемента из массива
+    };
+
     useEffect(() => {
-        let randomElements = getRandomElements(samples, 4); // получаем 4 случайных элемента
+        const fetchRandomElements = async () => {
+            let randomElements = getRandomElements(samples, 4);
+            randomElements = addRandomPrice(randomElements);
+            setItems(randomElements);
+            console.log(items);
+        };
 
-        randomElements = addRandomPrice(randomElements);
-
-        setItems(() => randomElements);
-        console.log(items);
+        fetchRandomElements();
         // eslint-disable-next-line
     }, [samples]);
 
     return (
-        <>
-            <div className='samples-container'>
-                <dir className='samples-container-title'>
-                    <h6>{title}</h6>
-                </dir>
-                <dir className='sample-container'>
-                    {items.map((sample) => (
-                        <Sample
-                            key={sample.idMeal}
-                            {...sample}
-                        />
-                    ))}
-                </dir>
+        <div className='samples-container'>
+            <div className='samples-container-title'>
+                <h6>{title}</h6>
             </div>
-        </>
+            <div className='sample-container'>
+                {items.map((sample) => (
+                    <Sample
+                        key={sample.idMeal}
+                        {...sample}
+                    />
+                ))}
+            </div>
+        </div>
     );
 };
