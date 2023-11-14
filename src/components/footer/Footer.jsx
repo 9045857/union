a09baf6ec1edecaf446dcbd565c2ea0d5d1c2dc5
9045857 <9045857@mail.ru>
2023-11-14 17:51:23 +0700

    Перекрасил футтер, в копирайт вставил Сан

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,14 @@
 function Footer() {
     return (
-        <footer className='page-footer #b0bec5 blue-grey lighten-3'>
+        <footer className='page-footer #ec407a pink lighten-1'>
             <div className='footer-copyright'>
                 <div className='container'>
-                    © {new Date().getFullYear()} Copyright Text
+                    © {new Date().getFullYear()} Copyright ООО "СанАзияТранс"
                     <a
                         className='grey-text text-lighten-4 right'
-                        href='https://github.com/9045857/react-food' target='_blank' rel="noreferrer"
+                        href='https://github.com/9045857/react-food'
+                        target='_blank'
+                        rel='noreferrer'
                     >
                         Repo
                     </a>
